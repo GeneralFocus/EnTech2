f74e7a2f68f24eb8a20a4f53e9973e91c9a699e4
generalfocus <generalfocus1@gmail.com>
2019-06-09 17:28:01 +0100

    First Commit

---
+++ app/src/main/java/com/capriquota/entech/bills/BillActivity.java
@@ -0,0 +1,15 @@
+package com.capriquota.entech.bills;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.capriquota.entech.R;
+
+public class BillActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bill);
+    }
+}
---
+++ app/src/main/java/com/capriquota/entech/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.capriquota.entech.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.capriquota.entech.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
